cf5c82340810e3163e355e3b0528ebc68f847f49
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 14:44:48 +0100

    Ajout de la première section de la page (titre , paragraphe , bouton , image )

--- src/Views/templates/home.php
+++ src/Views/templates/home.php
@@ -2,7 +2,17 @@
  /** Page d'accueil */
 ?>
 
-
-<h1>Accueil</h1>
+<div class="page">
+    <section class="section-one">
+        <div class="container-left">
+            <h1>Rejoigne nos lecteurs passionnés</h1>
+            <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
+            <a href="<?= BASE_URL; ?>login">Découvrir</a>
+        </div>
+        <div class="container-right">
+            <img src="<?= BASE_URL; ?>assets/accueil/image-first-section-accueil.png" alt=""
+        </div>
+    </section>
+</div>
 
 
