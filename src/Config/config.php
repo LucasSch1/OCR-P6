61ea04b024e60a39e93ab025d0fa50f18aa77ed4
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 14:44:16 +0100

    Modification du chemin pour la base url

--- src/Config/config.php
+++ src/Config/config.php
@@ -5,6 +5,6 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'ocr-p6');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('BASE_URL', '/OCR-P6/public');
+define('BASE_URL', '/OCR-P6/OCR-P6/public/');
 define('TEMPLATE_VIEW_PATH', dirname(__DIR__) . '/views/templates/'); // Le chemin vers les templates de vues.
 define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
