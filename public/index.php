47b48489e1a6f1e353c5b633c798ac1f3043c5ee
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 13:52:53 +0100

    Configuration de la page d'index pour appeler le bon controleur

--- public/index.php
+++ public/index.php
@@ -1,1 +1,28 @@
 <?php
+// Chargement des fichiers nécessaires
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Controllers\HomeController;
+use Services\Utils;
+
+
+// On récupère l'action demandée par l'utilisateur.
+// Si aucune action n'est demandée, on affiche la page d'accueil.
+$action = Utils::request('action', 'home');
+// Try catch global pour gérer les erreurs
+try {
+    // Pour chaque action, on appelle le bon contrôleur et la bonne méthode.
+    switch ($action) {
+        // Pages accessibles à tous.
+        case 'home':
+            $homeController = new HomeController();
+            $homeController->showHome();
+            break;
+    }
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche la page d'erreur.
+    $errorView = new View('Erreur');
+    $errorView->render('errorPage', ['errorMessage' => $e->getMessage()]);
+}
