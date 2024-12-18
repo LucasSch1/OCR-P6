9ef5a191702738011572b63733860886347d2e62
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 15:46:44 +0100

    Changement  du titre  h1

--- src/Views/templates/home.php
+++ src/Views/templates/home.php
@@ -5,7 +5,7 @@
 <div class="page">
     <section class="section-one">
         <div class="container-left">
-            <h1>Rejoigne nos lecteurs passionnés</h1>
+            <h1>Rejoignez nos<br> lecteurs passionnés</h1>
             <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
             <a href="<?= BASE_URL; ?>login">Découvrir</a>
         </div>
