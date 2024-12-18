bbc364a1458b0f6eaf8e0209fbb14b956d45dc91
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 15:48:47 +0100

    Ajout du vendor et de composer pour l'autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lucas/ocr-p6',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1b1b138d0f97163ce824cd9d081b42e5c2498992',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucas/ocr-p6' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1b1b138d0f97163ce824cd9d081b42e5c2498992',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
