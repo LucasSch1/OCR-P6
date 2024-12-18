2bf32879ddf1faa84bf963d5f469e808a4302c35
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-18 13:53:21 +0100

    Ajout de la classe Utils

---
+++ src/Services/Utils.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Classe utilitaire : cette classe ne contient que des méthodes statiques qui peuvent être appelées
+ * directement sans avoir besoin d'instancier un objet Utils.
+ * Exemple : Utils::redirect('home');
+ */
+
+namespace Services;
+
+class Utils
+{
+    public static function redirect(string $action, array $params = []) : void
+    {
+        $url = "index.php?action=$action";
+        foreach ($params as $paramName => $paramValue) {
+            $url .= "&$paramName=$paramValue";
+        }
+        header("Location: $url");
+        exit();
+    }
+
+    public static function request(string $variableName, mixed $defaultValue = null) : mixed
+    {
+        return $_REQUEST[$variableName] ?? $defaultValue;
+    }
+}
