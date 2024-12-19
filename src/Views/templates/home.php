69d0d4c7edeb392f45c006bc40e79caec30f4099
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-19 15:39:41 +0100

    Ajout de la deuxième et troisième section

--- src/Views/templates/home.php
+++ src/Views/templates/home.php
@@ -16,8 +16,82 @@
                 <img src="<?= BASE_URL; ?>assets/accueil/image-first-section-accueil.png" alt="">
                 <figcaption>Hamza</figcaption>
             </figure>
-
-
+        </div>
+    </section>
+    <section class="section-two">
+        <h2>Les derniers livres ajoutés</h2>
+        <div class="container-center">
+            <div class="card-container">
+                <div class="card-image">
+                    <img src="<?= BASE_URL; ?>assets/accueil/Mask group-5.png" alt="">
+                </div>
+                <div class="card-info">
+                    <h3>Esther</h3>
+                    <p>Alabaster</p>
+                    <div class="card-vendor">
+                        <p>Vendu par : CamilleClubLit</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card-image">
+                    <img src="<?= BASE_URL; ?>assets/accueil/Mask group-5.png" alt="">
+                </div>
+                <div class="card-info">
+                    <h3>The Kinfolk Table</h3>
+                    <p>Nathan Williams</p>
+                    <div class="card-vendor">
+                        <p>Vendu par : Nathalire</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card-image">
+                    <img src="<?= BASE_URL; ?>assets/accueil/Mask group-5.png" alt="">
+                </div>
+                <div class="card-info">
+                    <h3>Wabi Sabi</h3>
+                    <p>Beth Kempton</p>
+                    <div class="card-vendor">
+                        <p>Vendu par : Alexlecture</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card-image">
+                    <img src="<?= BASE_URL; ?>assets/accueil/Mask group-5.png" alt="">
+                </div>
+                <div class="card-info">
+                    <h3>Milk & honey</h3>
+                    <p>Rupi Kaur</p>
+                    <div class="card-vendor">
+                        <p>Vendu par : Hugo1990_12</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="btn-more-book">
+            <a href="<?= BASE_URL; ?>books">Voir tous les livres</a>
+        </div>
+    </section>
+    <section class="section-three">
+        <div class="container-top">
+            <h2>Comment ça marche ?</h2>
+            <p>Échanger des livres avec TomTroc c’est simple et amusant ! Suivez ces étapes pour commencer :</p>
+        </div>
+        <div class="container-center">
+            <div class="first-box">
+                <p>Inscrivez-vous gratuitement sur notre plateforme.</p>
+            </div>
+            <div class="second-box">
+                <p>Ajoutez les livres que vous souhaitez échanger à votre profil.</p>
+            </div>
+            <div class="third-box">
+                <p>Parcourez les livres disponibles chez d'autres membres.</p>
+            </div>
+            <div class="fourth-box">
+                <p>Proposez un échange et discutez avec d'autres passionnés de lecture.</p>
+            </div>
         </div>
     </section>
 </div>
