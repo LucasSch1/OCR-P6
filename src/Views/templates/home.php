6b26ca312d6ecca2e117530594aff416224b626d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-19 11:39:56 +0100

    Modification du bouton pour mettre dans une div approprié

--- src/Views/templates/home.php
+++ src/Views/templates/home.php
@@ -7,10 +7,17 @@
         <div class="container-left">
             <h1>Rejoignez nos<br> lecteurs passionnés</h1>
             <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
-            <a href="<?= BASE_URL; ?>login">Découvrir</a>
+            <div class="btn-container">
+                <a href="<?= BASE_URL; ?>login">Découvrir</a>
+            </div>
         </div>
         <div class="container-right">
-            <img src="<?= BASE_URL; ?>assets/accueil/image-first-section-accueil.png" alt=""
+            <figure>
+                <img src="<?= BASE_URL; ?>assets/accueil/image-first-section-accueil.png" alt="">
+                <figcaption>Hamza</figcaption>
+            </figure>
+
+
         </div>
     </section>
 </div>
