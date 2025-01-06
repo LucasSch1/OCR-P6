3dd77e40fc0d8843778fd27f1d871e87bc6a9d59
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-06 13:15:50 +0100

    Ajout de l'entité Book

---
+++ src/Models/Book.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Entité Book : un book est défini par son id , l'id du propriétaire , son titre, sa description, sa disponibilité et sa couverture
+ */
+
+namespace Lucas\OcrP6\Models;
+
+class Book
+{
+
+}
