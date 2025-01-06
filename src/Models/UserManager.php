715a1609aa834c26f9871fcfb3abae9815ff5059
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-06 15:52:04 +0100

    Ajout de la fonction pour récupérer un utilisateur via son email

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -3,6 +3,7 @@
 namespace Lucas\OcrP6\Models;
 
 use Lucas\OcrP6\Config\DBManager;
+use PDO;
 use Services\Utils;
 
 class UserManager
@@ -14,5 +15,26 @@ class UserManager
         $db = DBManager::getConnection();
         $stmt = $db->prepare("INSERT INTO user (email, password, username) VALUES (?, ?, ?)");
         $stmt->execute([$email, $passwordhash, $username]);
+    }
+
+
+    public function getUserByEmail(string $email): ?User
+    {
+        $db = DBManager::getConnection();
+
+        try {
+            $sql = "SELECT * FROM user WHERE email = :email";
+            $stmt = $db->prepare($sql);
+            $stmt->execute(['email' => $email]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($user) {
+                return new User($user); // Assurez-vous que le constructeur de User fonctionne avec un tableau
+            }
+        } catch (\PDOException $e) {
+            error_log("Erreur lors de la récupération de l'utilisateur : " . $e->getMessage());
+        }
+
+        return null;
     }
 }
