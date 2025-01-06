24863389490e53d60b05a6bf455490ae713d6b4d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-06 16:42:25 +0100

    Création de la page de compte

---
+++ src/Views/templates/account.php
@@ -0,0 +1,17 @@
+<?php
+/** Page du compte */
+?>
+
+
+<div class="page-account">
+    <h1>Mon compte</h1>
+    <div class="informations-cards">
+        <div class="container-left">
+
+        </div>
+        <div class="container-right">
+
+        </div>
+    </div>
+</div>
+
