03718373dd9b8c8d2c605c33cb3359354941332f
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-08 15:09:45 +0100

    Ajout des différentes variables pour afficher les informations

--- src/Views/templates/privateAccount.php
+++ src/Views/templates/privateAccount.php
@@ -1,8 +1,15 @@
 <?php
 /** Page du compte */
-session_start();
-?>
 
+use Lucas\OcrP6\Models\UserManager;
+
+session_start();
+$userManager = new UserManager();
+$userId = $_SESSION['user']['id'];
+$books = $userManager->getBookByUserId($userId);
+$total_books = $userManager->countBooksByUserId($userId);
+$registrationDate = $userManager->getUserRegistrationDate($userId);
+?>
 
 <div class="page-private-account">
     <h1>Mon compte</h1>
@@ -14,11 +21,11 @@ session_start();
                     <a href="" class="modify-button">modifier</a>
                     <hr class="separator">
                     <h2 class="username-container"><?php echo htmlspecialchars($_SESSION['user']['username']); ?></h2>
-                    <p class="date-member-container">Membre depuis <?php echo htmlspecialchars($_SESSION['user']['date_created']); ?></p>
+                    <p class="date-member-container"><?php echo htmlspecialchars($userManager->getMembershipDuration($registrationDate)); ?></p>
                     <h3 class="text-bibliotheque">BIBLIOTHEQUE</h3>
                     <div class="container-bibliotheque">
                         <img src="../public/assets/icon_book.svg" class="icon-container">
-                        <p class="book-number-container">4 livres</p>
+                        <p class="book-number-container"><?php echo htmlspecialchars($total_books); ?> livres</p>
                     </div>
                 </div>
             </div>
@@ -37,7 +44,36 @@ session_start();
             </div>
         </div>
         <div class="library-user">
-            <a>Bibliothèques de l'utilisateur</a>
+            <table>
+                <!-- En-têtes statiques en HTML -->
+                <thead>
+                <tr>
+                    <th>PHOTO</th>
+                    <th>TITRE</th>
+                    <th>AUTEUR</th>
+                    <th>DESCRIPTION</th>
+                    <th>DISPONIBILITE</th>
+                    <th>ACTION</th>
+
+                </tr>
+                </thead>
+                <tbody>
+                <!-- Données dynamiques en PHP -->
+                <?php foreach ($books as $book): ?>
+                    <tr>
+                        <td class="td-cover"><img src="../<?= htmlspecialchars($book['COVER'], ENT_QUOTES, 'UTF-8') ?>"></td>
+                        <td class="td-titre"><?= htmlspecialchars($book['TITLE'], ENT_QUOTES, 'UTF-8') ?></td>
+                        <td class="td-auteur"><?= htmlspecialchars($book['AUTHOR'], ENT_QUOTES, 'UTF-8') ?></td>
+                        <td class="td-description"><?= htmlspecialchars($shortDescription = substr($book['DESCRIPTION'], 0, 87).'...', ENT_QUOTES, 'UTF-8') ?></td>
+                        <td class="td-disponibilite"><?= htmlspecialchars($book['DISPONIBILITY'], ENT_QUOTES, 'UTF-8') ?></td>
+                        <td>
+                            <a href="">Editer</a>
+                            <a href="">Supprimer</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
     </div>
 
