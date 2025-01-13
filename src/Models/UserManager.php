25308096e3139ba5767a48393bce2a87023a3ac9
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-13 14:56:08 +0100

    Modification de la fonction d'inscription pour y passer un mot de passe hashé

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -3,18 +3,19 @@
 namespace Lucas\OcrP6\Models;
 
 use DateTime;
+use Exception;
 use Lucas\OcrP6\Config\DBManager;
 use PDO;
 use Services\Utils;
 
 class UserManager
 {
-    public function createUser($email, $password, $username)  {
+    public function createUser($email, $passwordHash, $username)  {
 
 
         $db = DBManager::getConnection();
         $stmt = $db->prepare("INSERT INTO user (email, password, username) VALUES (?, ?, ?)");
-        $stmt->execute([$email, $password, $username]);
+        $stmt->execute([$email, $passwordHash, $username]);
     }
 
 
@@ -29,7 +30,7 @@ class UserManager
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
             if ($user) {
-                return new User($user); // Assurez-vous que le constructeur de User fonctionne avec un tableau
+                return new User($user);
             }
         } catch (\PDOException $e) {
             error_log("Erreur lors de la récupération de l'utilisateur : " . $e->getMessage());
@@ -74,13 +75,13 @@ class UserManager
     }
 
     public function getMembershipDuration(string $registrationDate): string {
-        // Convertir la date d'inscription en objet DateTime
+
         try {
             $registrationDateTime = new DateTime($registrationDate);
         } catch (\DateMalformedStringException $e) {
 
         }
-        $currentDateTime = new DateTime(); // Date actuelle
+        $currentDateTime = new DateTime();
 
         // Calculer la différence
         $interval = $registrationDateTime->diff($currentDateTime);
@@ -99,12 +100,12 @@ class UserManager
 
 
     public function getUserRegistrationDate(int $userId): ?string {
-        $db = DBManager::getConnection(); // Connexion à la base de données
+        $db = DBManager::getConnection();
         $stmt = $db->prepare("SELECT DATE(date_creation) AS registration_date FROM user WHERE id = :id");
         $stmt->execute(['id' => $userId]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        return $result['registration_date'] ?? null; // Retourne la date ou null si introuvable
+        return $result['registration_date'] ?? null;
     }
 
     public function updateUser($emailUpdate, $passwordUpdate, $usernameUpdate ,$userId){
@@ -112,4 +113,24 @@ class UserManager
         $stmt = $db->prepare("UPDATE user SET email = :email , password = :password, username = :username WHERE id = :id");
         $stmt->execute(['email' => $emailUpdate, 'password' => $passwordUpdate, 'username' => $usernameUpdate , 'id' => $userId]);
     }
+
+    public function updateUserPicture($userId, $picturePath): void
+    {
+        // Connexion à la base de données
+        $db = DBManager::getConnection();
+
+        // Préparation de la requête SQL
+        $query = "UPDATE user SET picture = :picture WHERE id = :id";
+        $stmt = $db->prepare($query);
+
+        // Exécution de la requête avec les paramètres
+        $stmt->bindParam(':picture', $picturePath, PDO::PARAM_STR);
+        $stmt->bindParam(':id', $userId, PDO::PARAM_INT);
+
+        if (!$stmt->execute()) {
+            throw new Exception("Impossible de mettre à jour l'image de profil dans la base de données.");
+        }
+    }
+
+
 }
