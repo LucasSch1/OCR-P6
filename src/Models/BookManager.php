ef4825ffc9f5c8499b46cc0fe799cd37ceac22a3
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-14 16:52:07 +0100

    Ajout de la fonction pour mettre a jour le livre par rapport a son id

--- src/Models/BookManager.php
+++ src/Models/BookManager.php
@@ -2,6 +2,7 @@
 
 namespace Lucas\OcrP6\Models;
 
+use Exception;
 use Lucas\OcrP6\Config\DBManager;
 use PDO;
 
@@ -22,6 +23,37 @@ class BookManager
         catch (\PDOException $e) {
             error_log("Erreur lors de la récupération du livre de l'utilisateur : " . $e->getMessage());
             return [];
+        }
+    }
+
+    public function updateBookById($id , $titleUpdate, $authorUpdate, $commentaryUpdate, $disponibilityUpdate){
+        $db = DBManager::getConnection();
+
+        try {
+            $stmt = $db->prepare("UPDATE book SET title = :title , author = :author, description = :description , disponibility = :disponibility WHERE id = :id");
+            $stmt->execute(['title' => $titleUpdate, 'author' => $authorUpdate, 'description' => $commentaryUpdate , 'disponibility' => $disponibilityUpdate , 'id' => $id]);
+        }
+        catch (\PDOException $e) {
+            error_log("Une erreur lors de la mise a jour du livre est survenue : " . $e->getMessage());
+        }
+
+    }
+
+    public function updateBookPicture($bookId, $picturePath): void
+    {
+        // Connexion à la base de données
+        $db = DBManager::getConnection();
+
+        // Préparation de la requête SQL
+        $query = "UPDATE book SET cover = :cover WHERE id = :id";
+        $stmt = $db->prepare($query);
+
+        // Exécution de la requête avec les paramètres
+        $stmt->bindParam(':cover', $picturePath, PDO::PARAM_STR);
+        $stmt->bindParam(':id', $bookId, PDO::PARAM_INT);
+
+        if (!$stmt->execute()) {
+            throw new Exception("Impossible de mettre à jour l'image de couverture du livre dans la base de données.");
         }
     }
 
