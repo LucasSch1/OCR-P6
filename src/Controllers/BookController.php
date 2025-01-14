a68e2343af32434054038cce321f437dd466d252
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-14 16:51:41 +0100

    Ajout des fonctions pour mettre a jour le livre (texte + image)

--- src/Controllers/BookController.php
+++ src/Controllers/BookController.php
@@ -25,4 +25,62 @@ class BookController
         $view->render("updatePageBook", ['book' => $book]);
     }
 
+    public function updateBook(){
+        $id = Utils::request("idBookUpdate");
+        $titleUpdate = Utils::request("titlebookUpdate");
+        $authorUpdate = Utils::request("authorUpdate");
+        $commentaryUpdate = Utils::request("commentaryUpdate");
+        $disponibilityUpdate = Utils::request("choice-disponibility");
+
+        $bookManager = new BookManager();
+        $book = $bookManager->updateBookById($id , $titleUpdate, $authorUpdate, $commentaryUpdate, $disponibilityUpdate);
+
+        Utils::redirect("privateAccountUser");
+        exit();
+    }
+
+    public function updateBookPicture(): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['book-picture'])) {
+            if (!isset($_POST['bookId'])) {
+                throw new Exception("Une erreur est survenue");
+            }
+
+            $bookId = Utils::request("bookId");
+            $file = $_FILES['book-picture'];
+
+            $uploadDir = '../public/assets/book-images/';
+
+            if (!is_dir($uploadDir)) {
+                mkdir($uploadDir, 0755, true);
+            }
+            $fileBaseName = 'cover-image-' . $bookId;
+            array_map('unlink', glob($uploadDir . '/' . $fileBaseName . '.*'));
+
+            $fileExtension = pathinfo($file['name'], PATHINFO_EXTENSION);
+
+            $fileName = $fileBaseName . '.' . $fileExtension;
+
+            $uploadPath = $uploadDir . '/' . $fileName;
+
+            if (!move_uploaded_file($file['tmp_name'], $uploadPath)) {
+                throw new Exception("Impossible de déplacer le fichier téléchargé.");
+            }
+
+            $bookManager = new BookManager();
+            $bookManager->updateBookPicture($bookId, 'public/assets/book-images/' . $fileName);
+
+//            $_REQUEST['id'] = '/public/assets/book-images/' . $fileName;
+
+
+            Utils::redirect("privateAccountUser");
+            exit();
+        }
+
+        throw new Exception("Aucun fichier téléchargé.");
+    }
+
 }
