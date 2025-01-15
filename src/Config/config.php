933391a9a09ecb69daa3156d6c69c9e087eee933
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-15 15:54:55 +0100

    Modification des informations de connexion  à la base de données

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,10 +1,10 @@
 <?php
 namespace Config;
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.1.10');
 define('DB_NAME', 'ocr_p6');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'oeZMV48HZ!');
 define('BASE_URL', '/OCR-P6/OCR-P6/public/');
 define('TEMPLATE_VIEW_PATH', dirname(__DIR__) . '/views/templates/'); // Le chemin vers les templates de vues.
 define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
