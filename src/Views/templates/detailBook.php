c0902bd031ad37a9f359a961271a9c36641ec71a
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-16 15:33:43 +0100

    Ajout de la page de détail du livre

---
+++ src/Views/templates/detailBook.php
@@ -0,0 +1,40 @@
+<?php
+/*Page de détail d'un livre*/
+
+?>
+
+<section class="page-detail-book">
+    <div class="split-container">
+        <div class="image-container">
+            <img src="../<?= $book->getCover() ?>">
+        </div>
+        <div class="informations-container">
+            <div class="title-author-container">
+                <h1><?= $book->getTitle() ?></h1>
+                <p>par <?= $book->getAuthor() ?></p>
+            </div>
+            <hr>
+            <div class="main-container-description">
+                <h2>DESCRIPTION</h2>
+                <div class="container-description">
+                    <?=  nl2br($book->getDescription()) ?>
+                </div>
+            </div>
+            <div class="main-container-owner">
+                <h2>PROPRIÉTAIRE</h2>
+                <div class="container-owner">
+                    <a href="index.php?action">
+                        <div class="image-container-owner">
+                            <img src="../public/<?= $book->getUsername_picture() ?>" alt="<?= $book->getUsername_picture() ?>">
+                        </div>
+                        <p><?= $book->getUsername_owner() ?></p>
+                    </a>
+                </div>
+            </div>
+            <div class="btn-send-message">
+                <a href="index.php?action=showMessagerie" role="button">Envoyer un message</a>
+            </div>
+        </div>
+    </div>
+</section>
+
