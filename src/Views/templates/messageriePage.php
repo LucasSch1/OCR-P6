64a074374173e2b9832ea8a83d4914ec93666204
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-17 15:38:25 +0100

    Ajout de la page de messagerie

---
+++ src/Views/templates/messageriePage.php
@@ -0,0 +1,43 @@
+<?php
+/* Page de messagerie*/
+
+?>
+
+
+
+<section class="messagerie-page">
+    <div class="split-container">
+        <div class="users-container">
+            <h1>Messagerie</h1>
+            <div class="item-user-list">
+                <div class="item-user-container">
+                    <div class="image-user-container">
+                        <img>
+                    </div>
+                    <div class="user-information">
+                        <div class="split-container-user">
+                            <div class="user-name-container">
+                                <h2>Alexlecture</h2>
+                            </div>
+                            <div class="time-container">
+                                15:43
+                            </div>
+                        </div>
+                        <div class="intro-message-container">
+                            Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="conversation-container">
+            <div class="user-container">
+                <div class="image-user">
+                    <img>
+                </div>
+                <h2>Alexlecture</h2>
+            </div>
+
+        </div>
+    </div>
+</section>
