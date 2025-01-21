953baf9945a9e393245d98658a18e3776cd888e4
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-21 13:12:40 +0100

    Modification de la page de messagerie

--- src/Views/templates/messageriePage.php
+++ src/Views/templates/messageriePage.php
@@ -10,75 +10,102 @@
         <div class="users-container">
             <h1>Messagerie</h1>
             <div class="item-user-list">
-                <div class="item-user-container">
-                    <div class="image-user-container">
-                        <img src="../public/assets/profile-images/default-profile-image.png">
-                    </div>
-                    <div class="user-information">
-                        <div class="split-container-user">
-                            <div class="user-name-container">
-                                <h2>Alexlecture</h2>
-                            </div>
-                            <div class="time-container">
-                                15:43
-                            </div>
+                <a href="#" class="item-user-link">
+                    <div class="item-user-container">
+                        <div class="image-user-container">
+                            <img src="../public/assets/profile-images/default-profile-image.png">
                         </div>
-                        <div class="intro-message-container">
-                            <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item-user-container">
-                    <div class="image-user-container">
-                        <img src="../public/assets/profile-images/default-profile-image.png">
-                    </div>
-                    <div class="user-information">
-                        <div class="split-container-user">
-                            <div class="user-name-container">
-                                <h2>Alexlecture</h2>
+                        <div class="user-information">
+                            <div class="split-container-user">
+                                <div class="user-name-container">
+                                    <h2>Alexlecture</h2>
+                                </div>
+                                <div class="time-container">
+                                    15:43
+                                </div>
                             </div>
-                            <div class="time-container">
-                                15:43
+                            <div class="intro-message-container">
+                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
                             </div>
                         </div>
-                        <div class="intro-message-container">
-                            <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
+                    </div>
+                </a>
+                <a href="#" class="item-user-link">
+                    <div class="item-user-container">
+                        <div class="image-user-container">
+                            <img src="../public/assets/profile-images/default-profile-image.png">
                         </div>
-                    </div>
-                </div>
-                <div class="item-user-container">
-                    <div class="image-user-container">
-                        <img src="../public/assets/profile-images/default-profile-image.png">
-                    </div>
-                    <div class="user-information">
-                        <div class="split-container-user">
-                            <div class="user-name-container">
-                                <h2>Alexlecture</h2>
+                        <div class="user-information">
+                            <div class="split-container-user">
+                                <div class="user-name-container">
+                                    <h2>Alexlecture</h2>
+                                </div>
+                                <div class="time-container">
+                                    15:43
+                                </div>
                             </div>
-                            <div class="time-container">
-                                15:43
+                            <div class="intro-message-container">
+                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
                             </div>
                         </div>
-                        <div class="intro-message-container">
-                            <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
+                    </div>
+                </a>
+                <a href="#" class="item-user-link">
+                    <div class="item-user-container">
+                        <div class="image-user-container">
+                            <img src="../public/assets/profile-images/default-profile-image.png">
+                        </div>
+                        <div class="user-information">
+                            <div class="split-container-user">
+                                <div class="user-name-container">
+                                    <h2>Alexlecture</h2>
+                                </div>
+                                <div class="time-container">
+                                    15:43
+                                </div>
+                            </div>
+                            <div class="intro-message-container">
+                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
+
             </div>
         </div>
         <div class="conversation-container">
             <div class="user-container">
                 <div class="image-user">
-                    <img>
+                    <img src="../public/assets/profile-images/default-profile-image.png">
                 </div>
                 <h2>Alexlecture</h2>
             </div>
             <div class="messages-container">
-
+                <div class="item-message-sender">
+                    <div class="container-datetime">
+                        <p class="date-message">21.08</p>
+                        <p class="hours-message">15:44</p>
+                    </div>
+                    <div class="container-main-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor </p>
+                    </div>
+                </div>
+                <div class="item-message-receiver">
+                    <div class="container-datetime">
+                        <div class="image-user-receiver">
+                            <img src="../public/assets/profile-images/default-profile-image.png">
+                        </div>
+                        <p class="date-message">21.08</p>
+                        <p class="hours-message">15:44</p>
+                    </div>
+                    <div class="container-main-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor </p>
+                    </div>
+                </div>
             </div>
             <div class="input-button-container">
                 <input type="text" placeholder="Tapez votre message ici">
-                <a>Envoyer</a>
+                <a href="sendMessage" >Envoyer</a>
             </div>
         </div>
     </div>
