a97e84e117e884e044025fd87c49169cc66eca22
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-22 11:24:28 +0100

    Ajout des propriétes de l'entité Message

--- src/Models/Message.php
+++ src/Models/Message.php
@@ -9,4 +9,46 @@ namespace Lucas\OcrP6\Models;
 class Message
 {
 
+    private $id;
+
+    private $id_receiver;
+    private $id_sender;
+    private $content;
+
+    private $datetime;
+
+
+
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function getIdReceiver(){
+        return $this->id_receiver;
+    }
+    public function setIdReceiver($id_receiver){
+        $this->id_receiver = $id_receiver;
+    }
+    public function getIdSender(){
+        return $this->id_sender;
+    }
+    public function setIdSender($id_sender){
+        $this->id_sender = $id_sender;
+    }
+    public function getContent(){
+        return $this->content;
+    }
+    public function setContent($content){
+        $this->content = $content;
+    }
+
+    public function getDatetime(){
+        return $this->datetime;
+    }
+
+    public function setDatetime($datetime){
+        $this->datetime = $datetime;
+    }
 }
