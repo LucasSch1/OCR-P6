520330fd6684c70942bad0d0295470650be4c458
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-22 15:56:31 +0100

    Modification de la requete sql de la fonction de récupération des messages

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -154,20 +154,25 @@ class UserManager
     public function getUsersFromMessages($userId): array
     {
         $db = DBManager::getConnection();
-
         $query = "
-        SELECT 
-            user.id, 
-            user.username, 
-            user.picture, 
-            message.datetime AS last_message_date, 
-            message.content AS last_message_content
-        FROM user
-        INNER JOIN message
-            ON (user.id = message.id_sender AND message.id_receiver = :userId)
-            OR (user.id = message.id_receiver AND message.id_sender = :userId)
-        ORDER BY message.datetime DESC;
-        ";
+            SELECT 
+        user.id, 
+        user.username, 
+        user.picture, 
+        message.datetime AS last_message_date, 
+        message.content AS last_message_content
+    FROM user 
+    INNER JOIN message 
+        ON (user.id = message.id_sender AND message.id_receiver = :userId)
+        OR (user.id = message.id_receiver AND message.id_sender = :userId)
+    WHERE message.datetime = (
+        SELECT MAX(datetime) 
+        FROM message 
+        WHERE (id_sender = user.id AND id_receiver = :userId) 
+           OR (id_sender = :userId AND id_receiver = user.id)
+    )
+    ORDER BY message.datetime DESC;
+    ";
 
 
         $stmt = $db->prepare($query);
