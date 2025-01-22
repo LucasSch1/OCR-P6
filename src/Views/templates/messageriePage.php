952c5293c9f05fbcb322fba8a48726f8d97c21a3
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-22 11:26:02 +0100

    Modification de la page de messagerie pour un affichage dynamique avec des données de la base de données

--- src/Views/templates/messageriePage.php
+++ src/Views/templates/messageriePage.php
@@ -1,6 +1,5 @@
 <?php
 /* Page de messagerie*/
-
 ?>
 
 
@@ -10,103 +9,87 @@
         <div class="users-container">
             <h1>Messagerie</h1>
             <div class="item-user-list">
-                <a href="#" class="item-user-link">
-                    <div class="item-user-container">
-                        <div class="image-user-container">
-                            <img src="../public/assets/profile-images/default-profile-image.png">
-                        </div>
-                        <div class="user-information">
-                            <div class="split-container-user">
-                                <div class="user-name-container">
-                                    <h2>Alexlecture</h2>
+                <?php foreach ($listUsers as $user): ?>
+                    <form method="post" action="index.php?action=showMessageByUserId">
+                        <input type="hidden" name="user_id" value="<?=$user['id']?>">
+                        <button type="submit" class="item-user-link">
+                            <div class="item-user-container">
+                                <div class="image-user-container">
+                                    <img src="..<?= htmlspecialchars($user['picture'] ?: '/public/assets/profile-images/default-profile-image.png') ?>" alt="Photo de <?= htmlspecialchars($user['username']) ?>">
                                 </div>
-                                <div class="time-container">
-                                    15:43
-                                </div>
-                            </div>
-                            <div class="intro-message-container">
-                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="item-user-link">
-                    <div class="item-user-container">
-                        <div class="image-user-container">
-                            <img src="../public/assets/profile-images/default-profile-image.png">
-                        </div>
-                        <div class="user-information">
-                            <div class="split-container-user">
-                                <div class="user-name-container">
-                                    <h2>Alexlecture</h2>
-                                </div>
-                                <div class="time-container">
-                                    15:43
+                                <div class="user-information">
+                                    <div class="split-container-user">
+                                        <div class="user-name-container">
+                                            <h2><?= htmlspecialchars($user['username']) ?></h2>
+                                        </div>
+                                        <div class="time-container">
+                                            <?php
+                                            $lastMessageDate = strtotime($user['last_message_date']);
+                                            $now = time();
+                                            if (($now - $lastMessageDate) > 86400):
+                                                echo htmlspecialchars(date('d.m', $lastMessageDate));
+                                            else:
+                                                echo htmlspecialchars(date('H:i', $lastMessageDate));
+                                            endif;
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="intro-message-container">
+                                        <p><?= htmlspecialchars($user['last_message_content']) ?></p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="intro-message-container">
-                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="item-user-link">
-                    <div class="item-user-container">
-                        <div class="image-user-container">
-                            <img src="../public/assets/profile-images/default-profile-image.png">
-                        </div>
-                        <div class="user-information">
-                            <div class="split-container-user">
-                                <div class="user-name-container">
-                                    <h2>Alexlecture</h2>
-                                </div>
-                                <div class="time-container">
-                                    15:43
-                                </div>
-                            </div>
-                            <div class="intro-message-container">
-                                <p>Lorem ipsum dolor sit armetiejfijginetujbnesjibnjqernjinqeti</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-
+                        </button>
+                    </form>
+                    <a href="conversation.php?user_id=<?= htmlspecialchars($user['id']) ?>" class="item-user-link">
+                    </a>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="conversation-container">
+            <?php if ($selectedUser):?>
             <div class="user-container">
                 <div class="image-user">
-                    <img src="../public/assets/profile-images/default-profile-image.png">
+                    <img src="../<?= htmlspecialchars($selectedUser->getPicture() ?: '/public/assets/profile-images/default-profile-image.png') ?>" alt="Photo de <?= htmlspecialchars($selectedUser->getUsername()) ?>">
                 </div>
-                <h2>Alexlecture</h2>
+                <h2><?= htmlspecialchars($selectedUser->getUsername()) ?></h2>
             </div>
             <div class="messages-container">
-                <div class="item-message-sender">
-                    <div class="container-datetime">
-                        <p class="date-message">21.08</p>
-                        <p class="hours-message">15:44</p>
-                    </div>
-                    <div class="container-main-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor </p>
-                    </div>
-                </div>
-                <div class="item-message-receiver">
-                    <div class="container-datetime">
-                        <div class="image-user-receiver">
-                            <img src="../public/assets/profile-images/default-profile-image.png">
+                <?php foreach ($messages as $message):?>
+                    <?php if ($message['sender_id'] == $_SESSION['user']['id']): ?>
+                        <div class="item-message-sender">
+                            <div class="container-datetime">
+                                <p class="date-message"><?= htmlspecialchars(date('d.m', strtotime($message['time']))) ?></p>
+                                <p class="hours-message"><?= htmlspecialchars(date('H:i', strtotime($message['time']))) ?></p>
+                            </div>
+                            <div class="container-main-content">
+                                <p><?= htmlspecialchars($message['content']) ?></p>
+                            </div>
                         </div>
-                        <p class="date-message">21.08</p>
-                        <p class="hours-message">15:44</p>
-                    </div>
-                    <div class="container-main-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor </p>
-                    </div>
-                </div>
+                    <?php else: ?>
+                        <div class="item-message-receiver">
+                            <div class="container-datetime">
+                                <div class="image-user-receiver">
+                                    <img src="../<?= htmlspecialchars($selectedUser->getPicture() ?: '/public/assets/profile-images/default-profile-image.png') ?>" alt="Photo de <?= htmlspecialchars($selectedUser->getUsername()) ?>">
+                                </div>
+                                <p class="date-message"><?= htmlspecialchars(date('d.m', strtotime($message['time']))) ?></p>
+                                <p class="hours-message"><?= htmlspecialchars(date('H:i', strtotime($message['time']))) ?></p>
+                            </div>
+                            <div class="container-main-content">
+                                <p><?= htmlspecialchars($message['content']) ?></p>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
-            <div class="input-button-container">
-                <input type="text" placeholder="Tapez votre message ici">
-                <a href="sendMessage" >Envoyer</a>
-            </div>
+            <form class="input-button-container" method="post" action="index.php?action=sendMessage">
+                <input name="content" type="text" placeholder="Tapez votre message ici">
+                <input type="hidden" name="receiver_id" value="<?= htmlspecialchars($selectedUser->getId()) ?>">
+                <a type="submit" >Envoyer</a>
+            </form>
+            <?php else: ?>
+            <p>Sélectionnez un utilisateur pour commencer une conversation</p>
+            <?php endif; ?>
         </div>
     </div>
 </section>
