57ffeebdf7a0226e84c70b71fb79abaddf9d8240
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-23 11:21:26 +0100

    Modification du chemin de l'image

--- src/Views/templates/publicProfile.php
+++ src/Views/templates/publicProfile.php
@@ -8,7 +8,7 @@ var_dump($user);
     <div class="split-container">
         <div class="container-user-information">
             <div class="image-user-container">
-                <img src="../public/<?php echo $user->getPicture(); ?>">
+                <img src="../<?php echo $user->getPicture(); ?>">
             </div>
             <hr>
             <div class="information-user">
@@ -38,7 +38,12 @@ var_dump($user);
                 <tbody>
                 <?php foreach ($books as $book): ?>
                     <tr>
-                        <td class="td-cover"><img src="../<?= htmlspecialchars($book['COVER'], ENT_QUOTES, 'UTF-8') ?>"></td>
+                        <?php $url = "index.php?action=showDetailBook&idBook=" . $book['ID']; ?>
+                        <td class="td-cover">
+                            <a href="<?= $url ?>">
+                                <img src="../<?= htmlspecialchars($book['COVER'], ENT_QUOTES, 'UTF-8') ?>">
+                            </a>
+                        </td>
                         <td class="td-titre"><?= htmlspecialchars($book['TITLE'], ENT_QUOTES, 'UTF-8') ?></td>
                         <td class="td-auteur"><?= htmlspecialchars($book['AUTHOR'], ENT_QUOTES, 'UTF-8') ?></td>
                         <td class="td-description"><?= htmlspecialchars($shortDescription = substr($book['DESCRIPTION'], 0, 87).'...', ENT_QUOTES, 'UTF-8') ?></td>
