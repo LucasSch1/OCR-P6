655e7a3fcab72f3726575648933abc5ab345f75d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-28 11:51:33 +0100

    Ajout d'une condition pour afficher un message d'erreur lors de l'inscription

--- src/Views/templates/registerForm.php
+++ src/Views/templates/registerForm.php
@@ -5,14 +5,17 @@
 <section class="container-registration">
     <div class="container-left">
         <div class="container-form">
+            <?php if (!empty($error)): ?>
+                <p class="error"><?php echo htmlspecialchars($error); ?></p>
+            <?php endif; ?>
             <h1>Inscription</h1>
             <form method="POST" action="index.php?action=createUser">
                 <label for="pseudo">Pseudo</label>
-                <input type="text" name="username">
+                <input type="text" name="username" required>
                 <label for="email">Adresse mail</label>
-                <input type="text" name="email">
+                <input type="text" name="email" required>
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password">
+                <input type="password" name="password" required>
                 <input type="submit" class="submit-registration" value="S'inscrire">
             </form>
 
