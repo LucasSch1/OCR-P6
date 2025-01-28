1221e95744d01a77b10c8233b18c46985c6b7c9d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-28 11:49:01 +0100

    Modification de la page d'erreur

--- src/Views/templates/errorPage.php
+++ src/Views/templates/errorPage.php
@@ -6,4 +6,18 @@
 
 
 
-<h1>Désolé la page que vous demandez n'est pas disponible</h1>
+<section class="error-page">
+    <div class="main-container">
+        <div class="main-content">
+            <div class="error-text-image">
+                <h1>404</h1>
+            </div>
+            <div class="error-message">
+                <h2 class="text-main">Oops, vous vous êtes égaré en chemin</h2>
+                <p class="subtext-main">Pas de souci, cliquez ci-dessous pour revenir à la page d'accueil.</p>
+            </div>
+            <a href="index.php" class="btn-accueil">Revenir à l'accueil</a>
+        </div>
+    </div>
+</section>
+
