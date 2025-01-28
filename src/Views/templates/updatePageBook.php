502ba395ac4759176ee72ff66e8c340456285d28
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-28 15:32:24 +0100

    Ajout description image et icon

--- src/Views/templates/updatePageBook.php
+++ src/Views/templates/updatePageBook.php
@@ -4,18 +4,15 @@
  */
 
 use Lucas\OcrP6\Models\BookManager;
-
-
 $bookManager = new BookManager();
 $book = $bookManager->getBookById($_REQUEST['id']);
 $currentDisponibility = $book[0]['DISPONIBILITY'];
-var_dump($book[0]['ID']);
 ?>
 
 
 <section class="container-modification-book">
     <div class="return-container">
-        <a href="?action=privateAccountUser" class="back-link"><img src="../public/assets/arrow_return.svg"> retour</a>
+        <a href="?action=privateAccountUser" class="back-link"><img src="../public/assets/arrow_return.svg" alt="icon d'une flèche pour retourner en arrière"> retour</a>
     </div>
     <h1 class="title-page">Modifier les informations</h1>
     <div class="container-main">
@@ -23,7 +20,7 @@ var_dump($book[0]['ID']);
             <h2>Photo</h2>
             <form action="index.php?action=updateBookPicture" method="POST" enctype="multipart/form-data">
                 <div class="container-image-book">
-                    <img src="../<?php echo htmlspecialchars($book[0]['COVER']); ?>">
+                    <img src="../<?php echo htmlspecialchars($book[0]['COVER']); ?>" alt="Image du livre à mettre a jour">
                 </div>
                 <input type="hidden"  name="bookId" value="<?php echo htmlspecialchars($book[0]['ID']); ?>">
                 <input type="file" id="book-picture-upload" name="book-picture" accept="image/*" onchange="this.form.submit()">
@@ -34,13 +31,13 @@ var_dump($book[0]['ID']);
             <form method="POST" action="?action=updateBook">
                 <input type="hidden" name="idBookUpdate" value="<?php echo htmlspecialchars($book[0]['ID']); ?>">
                 <label for="titlebookUpdate">Titre</label>
-                <input type="text" name="titlebookUpdate" value="<?php echo htmlspecialchars($book[0]['TITLE']); ?>">
+                <input type="text" id="titlebookUpdate" name="titlebookUpdate" value="<?php echo htmlspecialchars($book[0]['TITLE']); ?>">
                 <label for="authorUpdate">Auteur</label>
-                <input type="text" name="authorUpdate" value="<?php echo htmlspecialchars($book[0]['AUTHOR']); ?>">
+                <input type="text" id="authorUpdate" name="authorUpdate" value="<?php echo htmlspecialchars($book[0]['AUTHOR']); ?>">
                 <label for="commentaryUpdate">Commentaire</label>
-                <textarea name="commentaryUpdate" class="commentary-container" rows="17"><?php echo htmlspecialchars($book[0]['DESCRIPTION']); ?></textarea>
+                <textarea id="commentaryUpdate" name="commentaryUpdate" class="commentary-container" rows="17"><?php echo htmlspecialchars($book[0]['DESCRIPTION']); ?></textarea>
 
-                <label for="choice-disponibility">Disponibilité</label>
+                <label for="disponibility-choice-book">Disponibilité</label>
                 <select id="disponibility-choice-book" name="choice-disponibility">
                     <option value="1" <?php echo $currentDisponibility == 1 ? 'selected': '' ?>>disponible</option>
                     <option value="0" <?php echo $currentDisponibility == 0 ? 'selected' : '' ?>>non disponible</option>
