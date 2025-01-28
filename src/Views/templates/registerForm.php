b24492ce0b84df84aebfa99f62dd1e86c719af2c
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-28 15:31:44 +0100

    Ajout des id pour lié au label les inputs

--- src/Views/templates/registerForm.php
+++ src/Views/templates/registerForm.php
@@ -11,18 +11,18 @@
             <h1>Inscription</h1>
             <form method="POST" action="index.php?action=createUser">
                 <label for="pseudo">Pseudo</label>
-                <input type="text" name="username" required>
+                <input type="text" id="pseudo" name="username" required>
                 <label for="email">Adresse mail</label>
-                <input type="text" name="email" required>
+                <input type="text" id="email" name="email" required>
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password" required>
-                <input type="submit" class="submit-registration" value="S'inscrire">
+                <input type="password" id="password" name="password" required>
+                <input type="submit" aria-label="S'inscrire (bouton vert)" class="submit-registration" value="S'inscrire">
             </form>
 
             <p>Déjà inscrit ? <a href="?action=connectionForm">Connectez-vous</a></p>
         </div>
     </div>
     <div class="container-right">
-        <img src="<?= BASE_URL; ?>assets/accueil/Mask group-2.png" alt="">
+        <img src="<?= BASE_URL; ?>assets/accueil/image-inscription.png" alt="Image de garde représentant une bibliothèque de livre">
     </div>
 </section>
