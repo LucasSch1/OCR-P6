c1ba4e9918b6a924e7e77c8ba270f839dfec1118
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-01-28 11:48:04 +0100

    Ajout d'une condition pour afficher les erreurs lors de la connexion

--- src/Views/templates/connectionForm.php
+++ src/Views/templates/connectionForm.php
@@ -5,12 +5,15 @@
 <section class="container-connection">
     <div class="container-left">
         <div class="container-form">
+            <?php if (!empty($error)): ?>
+                <p class="error"><?php echo htmlspecialchars($error); ?></p>
+            <?php endif; ?>
             <h1>Connexion</h1>
             <form method="POST" action="?action=connectUser">
                 <label for="email">Adresse mail</label>
-                <input type="text" name="email">
+                <input type="text" name="email" required>
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password">
+                <input type="password" name="password" required>
                 <input type="submit" class="submit-connection" value="Connexion">
             </form>
 
